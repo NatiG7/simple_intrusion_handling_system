70a21f4583bb5410d138e3a781a52a5918b708f2
Nati <netanelgoral@gmail.com>
2025-04-05 17:39:22 +0300

    refac: reformat prints on single capture script

--- testing/single_capture.py
+++ testing/single_capture.py
@@ -4,26 +4,40 @@ from scapy.all import sniff
 # Function to process the captured packet and print detailed info
 def packet_callback(packet):
     print("\nPacket Captured:")
+    print("=" * 60)
     
     # Print details of IP layer (if present)
     if packet.haslayer('IP'):
-        print(f"IP Version: {packet['IP'].version}")
-        print(f"Source IP: {packet['IP'].src}")
-        print(f"Destination IP: {packet['IP'].dst}")
-        print(f"Packet Length: {packet['IP'].len} bytes")
-        print(f"Identification Field: {packet['IP'].id}")
-        print(f"Header Checksum: {packet['IP'].chksum}")
+        print("Packet has IP Layer:")
+        print(f"\tIP Version: {packet['IP'].version}")
+        print(f"\tSource IP: {packet['IP'].src}")
+        print(f"\tDestination IP: {packet['IP'].dst}")
+        print(f"\tPacket Length: {packet['IP'].len} bytes")
+        print(f"\tIdentification Field: {packet['IP'].id}")
+        print(f"\tHeader Checksum: {packet['IP'].chksum}")
+        print("=" * 60)
 
     # Print details of TCP layer (if present)
     if packet.haslayer('TCP'):
-        print(f"Source Port: {packet['TCP'].sport}")
-        print(f"Destination Port: {packet['TCP'].dport}")
-        print(f"Sequence Number: {packet['TCP'].seq}")
-        print(f"TCP Header Size: {packet['TCP'].dataofs * 4} bytes")
-        print(f"Reserved Bits: {packet['TCP'].reserved}")
-        print(f"TCP Flags: {packet['TCP'].flags}")
-        print(f"TCP Checksum: {packet['TCP'].chksum}")
-        print(f"TCP Window Size: {packet['TCP'].window}")
+        print("Packet has TCP Layer:")
+        print(f"\tSource Port: {packet['TCP'].sport}")
+        print(f"\tDestination Port: {packet['TCP'].dport}")
+        print(f"\tSequence Number: {packet['TCP'].seq}")
+        print(f"\tTCP Header Size: {packet['TCP'].dataofs * 4} bytes")
+        print(f"\tReserved Bits: {packet['TCP'].reserved}")
+        print(f"\tTCP Flags: {packet['TCP'].flags}")
+        print(f"\tTCP Checksum: {packet['TCP'].chksum}")
+        print(f"\tTCP Window Size: {packet['TCP'].window}")
+        print("=" * 60)
+    
+    # Print details of ICMP layer (if present)
+    if packet.haslayer('ICMP'):
+        print("=" * 60)
+        print("ICMP Layer Found:")
+        print("=" * 60)
+        
+    else:
+        print(packet.summary())
 
 # Capture a single packet on the default interface
 sniff(count=1, prn=packet_callback)
